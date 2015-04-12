a389f89c6dfef904608b82198f99a4be70cd18ea
Jonathan Alfonso <alfonsojon1997@gmail.com>
2015-04-12 14:21:19 -0500

    Update gen-desktop.sh
    Revert spawndialog, not necessary in this usage.

--- gen-desktop.sh
+++ gen-desktop.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-spawndialog () {
-	[[ -x "$(which zenity)" ]] || {
-		printf '%b\n' "Missing dependency! Please install \"zenity\", then try again."
-		exit 1
-	}
-	zenity \
-		--no-wrap \
-		--window-icon="$RBXICON" \
-		--title='Roblox Linux Wrapper' \
-		--"$1" \
-		--text="$2"
-}
-
-
 # This script generates a .desktop file based on the current path of the wrapper
 WRAPPER_DIR=$(pwd)
 
@@ -36,4 +22,4 @@ Name=Roblox for All
 Exec=xdg-open "http://www.roblox.com/Groups/group.aspx?gid=292611"
 ' "$WRAPPER_DIR" "$WRAPPER_DIR" | tee roblox.desktop
 
-spawndialog info 'Finished. You can now install this desktop file via the command: xdg-desktop-menu install --novendor --mode user roblox.desktop'
+printf '%b\n' 'Finished. You can now install this desktop file via the command: xdg-desktop-menu install --novendor --mode user roblox.desktop'
