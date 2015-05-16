240fad113c3b455626deed763d05a16f0d893f27
James Lu <GLolol1@hotmail.com>
2015-05-16 12:14:26 -0700

    Move deb-release.sh into debian/
    We don't need to ship this in an official tarball.

--- debian/deb-release.sh
+++ debian/deb-release.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [[ "$(basename $(pwd))" == "debian" ]]; then
+	cd ..
+fi
+
 if [[ -d ".git" ]]; then
 	VERSION=$(git describe --tags | sed 's/^v//' | sed 's/-/+/g')
 	dch -v "${VERSION}" --distribution unstable "Automatic build."
