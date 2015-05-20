83052e51337e0b9cdcd0feef2cb9dff9e4b26a0c
James Lu <GLolol1@hotmail.com>
2015-05-19 22:29:27 -0700

    debian/deb-release.sh: more verbose messages, auto commit

--- debian/deb-release.sh
+++ debian/deb-release.sh
@@ -6,7 +6,10 @@ fi
 
 if [[ -d ".git" ]]; then
 	VERSION=$(git describe --tags | sed 's/^v//' | sed 's/-/+/g')
-	dch -v "${VERSION}" --distribution unstable "Automatic build."
+	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	BUILD_MESSAGE="Build Git branch ${BRANCH} revision ${VERSION}."
+	dch -v "${VERSION}" --distribution unstable "${BUILD_MESSAGE}"
+	git commit --author="RLW Automatic Builder <packages-admin@overdrive.pw>" debian/changelog -m "${BUILD_MESSAGE}"
 else
 	printf '%b\n' 'Must be in Git tree to update version automatically.'
 fi
