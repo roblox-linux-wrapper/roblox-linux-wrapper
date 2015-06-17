b1f89ea4c4fe91cfd8a4c6fc890c81ce46611e9d
James Lu <GLolol1@hotmail.com>
2015-06-16 17:16:51 -0700

    debian/: make deb-release.sh auto-tag as well

--- debian/deb-release.sh
+++ debian/deb-release.sh
@@ -5,12 +5,18 @@ if [[ "$(basename $(pwd))" == "debian" ]]; then
 fi
 
 if [[ -d ".git" ]]; then
-	VERSION=$(git describe --tags | sed 's/^v//' | sed 's/-/+/g')
+	if [[ "$1" == "commit" ]]; then
+		VERSION=$(date "+%Y.%m.%d")
+	else
+		VERSION=$(git describe --tags | sed 's/^v//' | sed 's/-/+/g')
+	fi
 	BRANCH=$(git rev-parse --abbrev-ref HEAD)
 	BUILD_MESSAGE="Build Git branch ${BRANCH} revision ${VERSION}."
 	dch -v "${VERSION}" --distribution unstable "${BUILD_MESSAGE}"
 	if [[ "$1" == "commit" ]]; then
 		git commit --author="RLW Automatic Builder <packages-admin@overdrive.pw>" debian/changelog -m "${BUILD_MESSAGE}"
+		printf '%b\n' "Tagging as ${VERSION}."
+		git tag -f "${VERSION}"
 	fi
 else
 	printf '%b\n' 'Must be in Git tree to update version automatically.'
