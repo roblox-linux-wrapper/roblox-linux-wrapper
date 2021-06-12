4b251d7ae30c8a408c0f471de137ae5152e18d01
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-06-12 16:46:08 +0300

    Added fallback to player.sh

--- data/roblox-player.sh
+++ data/roblox-player.sh
@@ -6,13 +6,17 @@ if [ -f "rlw-core.sh" ]; then
 	printf "Sourcing rlw-core.sh\n"
 	source "rlw-core.sh"
 else
-	zenity \
-		--no-wrap \
-		--window-icon="$RBXICON" \
-		--title="version-unknown" \
-		--error \
-		--text="Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n" 2&> /dev/null
-    exit 1
+	if command -v zenity >/dev/null 2>&1 ; then
+		zenity \
+			--no-wrap \
+			--window-icon="$RBXICON" \
+			--title="Roblox Linux Wrapper" \
+			--error \
+			--text="Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n" 2&> /dev/null	
+	else
+		 kdialog  --error "Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n"  --title "Roblox Linux Wrapper" 2&> /dev/null
+	fi
+	exit 1
 fi
 
 rwine "$(find "$HOME/.local/share/wineprefixes/roblox-wine" -name RobloxPlayerLauncher.exe)" "$1"
