13f48d000c98d21bc4431585ae37aa3db19b182e
ohno9119 <81485194+ohno9119@users.noreply.github.com>
2021-06-13 10:47:43 +0300

    Update roblox-studio.sh

--- data/roblox-studio.sh
+++ data/roblox-studio.sh
@@ -9,12 +9,12 @@ else
 	if command -v zenity >/dev/null 2>&1 ; then
 		zenity \
 			--no-wrap \
-			--window-icon="$RBXICON" \
+			--window-icon=roblox\
 			--title="Roblox Linux Wrapper" \
 			--error \
 			--text="Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n" 2&> /dev/null	
 	else
-		 kdialog  --error "Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n"  --title "Roblox Linux Wrapper" 2&> /dev/null
+		 kdialog --icon roblox --error "Missing rlw-core: try reinstalling rlw using the main script. If this problem presists, please report an issue to our GitHub page.\n"  --title "Roblox Linux Wrapper" 2&> /dev/null
 	fi
     exit 1
 fi
