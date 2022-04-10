8281161b9824cdacf692c6a8fba639d8fd71b087
Daniel <code@devmind.org>
2022-04-10 17:51:39 +0200

    #197 * Only use first find result for RobloxPlayerLauncher.exe

--- data/roblox-player.sh
+++ data/roblox-player.sh
@@ -15,4 +15,4 @@ else
     exit 1
 fi
 
-rwine "$(find "$HOME/.local/share/wineprefixes/roblox-wine" -name RobloxPlayerLauncher.exe)" "$1"
+rwine "$(find "$HOME/.local/share/wineprefixes/roblox-wine" -name RobloxPlayerLauncher.exe -print -quit)" "$1"
