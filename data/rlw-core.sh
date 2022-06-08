b0413faf4827e02b31c9c7bb3c50c9979e4edd50
XOMUS <93247740+XOMUS@users.noreply.github.com>
2022-06-08 17:37:46 +0500

    Update rlw-core.sh

--- data/rlw-core.sh
+++ data/rlw-core.sh
@@ -46,6 +46,9 @@ winechooser () {
 		'Std roblox wine')
 			# Here, we will literally save '$(which wine)' as the path
 			# so it changes dynamically and isn't immediately evaluated.
+			mkdir $HOME/.winexe
+			wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1q4l4FvUj6bfMZGBEUXnsOPUgBxwUMXTr' -O wine.tar.xz
+			tar -C $HOME/.winexe/ -xf $HOME/.winexe/wine.tar.xz 
 			WINE=$HOME/.winexe/bin/wine
 			WINESERVER=$HOME/.winexe/bin/wineserver
 			for x in "$WINE" "$WINESERVER"; do
